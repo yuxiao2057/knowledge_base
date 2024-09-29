a3c3cc681d81b3c68636801a6f50be055ff0c2ce
Yuxiao <luyuxiao2000@gmail.com>
2024-09-30 00:11:38 +0200

    nms new try

--- dectection/nms_try.py
+++ dectection/nms_try.py
@@ -1,16 +1,7 @@
 import numpy as np
 
 def nms(bboxes, threshold):
-    """
-
-    Args:
-        bboxes (np.array): bboxes with shape (N, 5), 5 means (x1, y1, x2, y2, score)
-        threshold (float): threshold for nms
-
-    Returns:
-        output_bboxes: output bboxes after nms
-        
-    """
+    
     if len(bboxes) == 0:
         return []
     
@@ -18,11 +9,14 @@ def nms(bboxes, threshold):
     y1 = bboxes[:, 1]
     x2 = bboxes[:, 2]
     y2 = bboxes[:, 3]
+    scores = bboxes[:, 4]
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
     
-    sorted_idx = np.argsort(bboxes[:, 4])[::-1]
+    sorted_idx = np.argsort(scores)[::-1]
     output_bboxes = []
+    
     while sorted_idx.size > 0:
+        
         best = sorted_idx[0]
         output_bboxes.append(bboxes[best])
         
@@ -34,22 +28,22 @@ def nms(bboxes, threshold):
         w_overlap = np.maximum(0, x2_overlap - x1_overlap + 1)
         h_overlap = np.maximum(0, y2_overlap - y1_overlap + 1)
         area_overlap = w_overlap * h_overlap
+        
         ious = area_overlap / (areas[sorted_idx[1:]] + areas[best] - area_overlap)
         
-        left_boxes = np.where(ious < threshold)[0]
-        sorted_idx = sorted_idx[left_boxes + 1]
-    
-    return output_bboxes
+        left_bboxes = np.where(ious < threshold)[0]
+        sorted_idx = sorted_idx[left_bboxes + 1]
+        
+    return np.array(output_bboxes)
 
 if __name__ == '__main__':
+    
     np.random.seed(10)
     x1 = np.random.uniform(low = 0, high = 50, size= (100, 1))
-    x2 = np.random.uniform(low = 150, high = 200, size= (100, 1))
     y1 = np.random.uniform(low = 0, high = 50, size= (100, 1))
+    x2 = np.random.uniform(low = 150, high = 200, size= (100, 1))
     y2 = np.random.uniform(low = 150, high = 200, size= (100, 1))
-    scores = np.random.rand(100)
-    scores = scores[:, np.newaxis]
-    bboxes = np.hstack([x1, y1, x2, y2, scores])
-    output_bboxes = nms(bboxes, threshold=0.5)
-    for bbox in output_bboxes:
-        print(bbox.tolist())
+    scores = np.random.rand(100, 1)
+    bboxes = np.hstack((x1, y1, x2, y2, scores))
+    output_bboxes = nms(bboxes, 0.7)
+    print(output_bboxes)
