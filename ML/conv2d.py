bfca2eaea394923e3012dc0f5a27c21f86e4cd3c
Yuxiao <luyuxiao2000@gmail.com>
2024-10-28 11:12:56 +0100

    new conv2d

--- ML/conv2d.py
+++ ML/conv2d.py
@@ -1,26 +1,28 @@
-import numpy as np 
-def conv2d(img, in_channels, out_channels ,kernels, bias, stride=1, padding=0):
-    N, C, H, W = img.shape 
+import numpy as np
+
+def conv2d(img, in_channels, out_channels, kernels, bias, stride=1, padding=0):
+    N, C, H, W = img.shape
     kh, kw = kernels.shape
     p = padding
     assert C == in_channels, "kernels' input channels do not match with img"
  
     if p:
-        img = np.pad(img, ((0,0),(0,0),(p,p),(p,p)), 'constant') # padding along with all axis
- 
-    out_h = (H + 2*padding - kh) // stride + 1
-    out_w = (W + 2*padding - kw) // stride + 1
- 
+        img = np.pad(img, ((0,0),(0,0),(p,p),(p,p)), 'constant')  # padding along all axes
+
+    out_h = (H + 2 * padding - kh) // stride + 1
+    out_w = (W + 2 * padding - kw) // stride + 1
     outputs = np.zeros([N, out_channels, out_h, out_w])
-    # print(img)
-    for n in range(N):
-        for out in range(out_channels):
-            for i in range(in_channels):
-                for h in range(out_h):
-                    for w in range(out_w):
-                        for x in range(kh):
-                            for y in range(kw):
-                                outputs[n][out][h][w] += img[n][i][h * stride + x][w * stride + y] * kernels[x][y]
-                if i == in_channels - 1:
-                    outputs[n][out][:][:] += bias[n][out]
+
+    for n in range(N):  # Batch size
+        for out in range(out_channels):  # Output channels
+            for i in range(in_channels):  # Input channels
+                for h in range(out_h):  # Output height
+                    for w in range(out_w):  # Output width
+                        # 提取当前卷积窗口
+                        img_patch = img[n, i, h * stride: h * stride + kh, w * stride: w * stride + kw]
+                        # 使用矩阵逐元素乘法，然后求和
+                        outputs[n, out, h, w] += np.sum(img_patch * kernels)
+            # 加上偏置
+            outputs[n, out, :, :] += bias[n][out]
+    
     return outputs
